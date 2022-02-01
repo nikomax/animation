b1355ef739522d01b6e0f5a78fa42f8d67828751
max <maks@coderiver.com.ua>
2022-02-01 14:33:28 +0200

    add gsap

--- app.js
+++ app.js
@@ -1,37 +1,34 @@
-const circle = document.querySelector('.js-circle');
-const section = document.querySelector('.js-i-section');
-const gasContent = document.querySelector('.js-gas-content');
-let directionTop = false
-let lastScroll = 0;
+gsap.registerPlugin(ScrollTrigger);
 
-const setClasses = () => {
-	let currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
-	if (currentScroll > 0 && lastScroll <= currentScroll){
-		lastScroll = currentScroll;
-		directionTop = false;
-	} else {
-		lastScroll = currentScroll;
-		directionTop = true;
-	}
-
-	if (section.getBoundingClientRect().top - window.innerHeight < 0) {
-		section.classList.add('is-active');
-	} else {
-		section.classList.remove('is-active');
-	}
-
-	if (gasContent.getBoundingClientRect().top - window.innerHeight/2 < 0) {
-		circle.classList.add('is-gas');
-		section.classList.add('theme-changed');
-	} else if (directionTop) {
-		circle.classList.remove('is-gas');
-		section.classList.remove('theme-changed');
-	}
-}
+ScrollTrigger.saveStyles('.js-circle-strom, .js-circle-gas, .js-overwrap, .js-heart, .js-fire, .js-rays, .js-strom-title, .js-gas-title, .js-panel, .js-bg, .js-plus, .js-circles');
 
 window.onload = () => {
-	setClasses();
-	window.addEventListener('scroll', () => {
-		setClasses()
+	ScrollTrigger.matchMedia({
+		'(min-width: 960px)': () => {
+			gsap.timeline({
+				defaults: { force3D: true, willChange: "transform, opacity, mask-image" },
+				ease: 'Power1.easeInOut',
+				scrollTrigger: {
+					id: 'trigger1',
+					trigger: '.js-container',
+					pin: true,
+					start: 'top top',
+					end: '+=800',
+					scrub: true
+				}
+			})
+				.to('.js-plus', {opacity: 0}, 0)
+				.to('.js-circle-strom', {x: 37, duration: 0.5}, 0)
+				.to('.js-circle-gas', {x: -37, duration: 0.5}, 0)
+				.to('.js-circles', {scale: 1, opacity: 0, duration: 0.5}, 0)
+				.to('.js-bg', {opacity: 1, duration: 0.5}, 0)
+				.to('.js-fire', {maskImage: 'linear-gradient(90deg, transparent 30%, rgba(0, 0, 0, 1) 60%)', duration: 0.5}, 0)
+				.to('.js-rays', {maskImage: 'linear-gradient(-90deg, transparent 30%, rgba(0, 0, 0, 1) 60%)', duration: 0.5}, 0)
+				.to('.js-overwrap', {x: 0, duration: 0.25}, 0.25)
+				.to('.js-strom-title', {x: -20}, 0.3)
+				.to('.js-gas-title', {x: 10}, 0.3)
+				.to('.js-heart', {opacity: 1, scale: 1}, 0.6)
+				.to('.js-panel', {opacity: 1, y: 0}, 0.6)
+		}
 	})
 }
