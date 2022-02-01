e39379b042418ffd119ee9aee438c1326db99507
max <maks@coderiver.com.ua>
2022-02-01 17:26:35 +0200

    add changes

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.saveStyles('.js-circle-strom, .js-circle-gas, .js-overwrap, .js-heart, .js-fire, .js-rays, .js-strom-title, .js-gas-title, .js-panel, .js-bg, .js-plus, .js-circles');
+ScrollTrigger.saveStyles('.js-circle-strom, .js-circle-gas, .js-overwrap, .js-heart, .js-fire, .js-rays, .js-strom-title, .js-gas-title, .js-panel, .js-bg, .js-plus, .js-circles, .js-text-top, .js-text-bottom, .i-circle__bg');
 
 window.onload = () => {
+	const strom = document.querySelector('.js-circle-strom');
+	const gas = document.querySelector('.js-circle-gas');
+	const switchers = document.querySelectorAll('.js-switcher-btn');
+	const buttons = document.querySelectorAll('.js-panel-btn');
+	switchers.forEach(switcher => {
+		switcher.addEventListener('click', function () {
+			switchers.forEach(item => { item.classList.remove('is-active')});
+			buttons.forEach(item => { item.classList.remove('is-active')});
+			this.classList.add('is-active');
+			const index = this.dataset.index;
+			document.querySelector(`.js-panel-btn[data-index="${index}"]`).classList.add('is-active');
+		})
+	})
 	ScrollTrigger.matchMedia({
 		'(min-width: 960px)': () => {
+			gsap.timeline({
+				ease: 'Power1.easeInOut',
+				scrollTrigger: {
+					trigger: '.js-container',
+					pin: false,
+					start: 'top center',
+					end: 'top top',
+					scrub: true,
+					onEnter: () => {
+						strom.classList.remove('is-active');
+						gas.classList.remove('is-active');
+					},
+					onEnterBack: () => {
+						strom.classList.add('is-active');
+						gas.classList.add('is-active');
+					},
+					onLeave: () => {
+						strom.classList.add('is-active');
+						gas.classList.add('is-active');
+					},
+					onLeaveBack: () => {
+						strom.classList.remove('is-active');
+						gas.classList.remove('is-active');
+					}
+				}
+			})
+				.fromTo('.i-circle__bg', {scale: 2}, {scale: 1, clearProps:"all"}, 0)
+				.fromTo('.js-circle-strom', {x: -323}, {x: -123, clearProps:"all"}, 0)
+				.fromTo('.js-circle-gas', {x: 323}, {x: 123, clearProps:"all"}, 0)
+
 			gsap.timeline({
 				defaults: { force3D: true, willChange: "transform, opacity, mask-image" },
 				ease: 'Power1.easeInOut',
 				scrollTrigger: {
-					id: 'trigger1',
 					trigger: '.js-container',
 					pin: true,
 					start: 'top top',
@@ -27,6 +69,8 @@ window.onload = () => {
 				.to('.js-overwrap', {x: 0, duration: 0.25}, 0.25)
 				.to('.js-strom-title', {x: -20}, 0.3)
 				.to('.js-gas-title', {x: 10}, 0.3)
+				.to('.js-text-top', {opacity: 0, duration: 0.1}, 0.6)
+				.to('.js-text-bottom', {opacity: 1, duration: 0.1}, 0.6)
 				.to('.js-heart', {opacity: 1, scale: 1}, 0.6)
 				.to('.js-panel', {opacity: 1, y: 0}, 0.6)
 		}
